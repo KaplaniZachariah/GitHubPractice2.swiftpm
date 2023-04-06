90a4427866dff8e9de66cd58ef3449c30ac26eaf
KaplaniZachariah <zkaplani4201@stu.d214.org>
2023-04-06 14:33:47 -0500

    commit: added the summary view and added the sentence inside the summary view

---
+++ SummaryView.swift
@@ -0,0 +1,16 @@
+//
+//  SummaryView.swift
+//  GitHubPractice2
+//
+//  Created by Zach kaplani  on 4/6/23.
+//
+
+import SwiftUI
+
+struct SummaryView: View {
+    var body: some View {
+        VStack{
+            Text("My dog ran to the black house cat and ate it")
+        }
+    }
+}
