7ea18013cf6a734d969f321058483287ebb0cbad
KaplaniZachariah <zkaplani4201@stu.d214.org>
2023-04-06 14:20:43 -0500

    added the adjective view file to the project and added a navigation link to the page adjective view

---
+++ AdjectiveView.swift
@@ -0,0 +1,16 @@
+//
+//  AdjectiveView.swift
+//  GitHubPractice2
+//
+//  Created by Zach kaplani  on 4/6/23.
+//
+
+import SwiftUI
+
+
+struct AdjectiveView: View {
+    var body: some View {
+        
+        NavigationLink(SummaryView, value: $SummaryView)
+    }
+}
